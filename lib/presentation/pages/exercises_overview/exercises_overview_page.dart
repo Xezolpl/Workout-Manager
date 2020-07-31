9dce5cbf9baf5e55fef7ad33fb36cb2e886c1e4b
XezolPL <XezolPL@wp.pl>
2020-07-31 15:58:23 +0200

    Authorization bug fixed

--- lib/presentation/pages/exercises_overview/exercises_overview_page.dart
+++ lib/presentation/pages/exercises_overview/exercises_overview_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:workout_manager/injection.dart';
-import 'package:workout_manager/presentation/bloc/auth/auth_bloc.dart';
 import 'package:workout_manager/presentation/bloc/exercises/exercises_bloc.dart';
 import 'package:workout_manager/presentation/pages/widgets/bottom_nav_bar.dart';
 import 'package:workout_manager/presentation/routes/router.gr.dart';
@@ -15,62 +14,52 @@ class ExercisesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthBloc, AuthState>(
-        listener: (context, state) {
-          state.maybeMap(
-            unauthenticated: (_) =>
-                Router.navigator.pushReplacementNamed(Routes.signInPage),
-            orElse: () {},
-          );
-        },
-        child: BlocProvider(
-          create: (context) =>
-              getIt<ExercisesBloc>()..add(ExercisesEvent.watchAllStarted()),
-          child: Scaffold(
-            appBar: AppBar(),
-            bottomNavigationBar: bNavBar,
-            body: BlocBuilder<ExercisesBloc, ExercisesState>(
-              builder: (context, state) {
-                return state.map(
-                  initial: (_) => Container(),
-                  loadInProgress: (_) => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                  loadSuccess: (state) {
-                    return GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: MediaQuery.of(context).orientation ==
-                                  Orientation.landscape
-                              ? 3
-                              : 2),
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: GestureDetector(
-                              onTap: () => bNavBar.changePage(1,
-                                  exercise: state.exercises[index]),
-                              child: ExerciseCard(
-                                  exercise: state.exercises[index])),
-                        );
-                      },
-                      itemCount: state.exercises.length,
-                    );
-                  },
-                  loadFailure: (state) {
-                    return Container(
-                      child: Text('FAILURE'),
-                    );
-                  },
-                );
-              },
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                Router.navigator.pushNamed(Routes.exerciseFormPage,
-                    arguments: ExerciseFormPageArguments(editedExercise: null));
-              },
-              child: Center(child: Icon(Icons.add)),
-            ),
+    return BlocProvider(
+        create: (context) =>
+            getIt<ExercisesBloc>()..add(ExercisesEvent.watchAllStarted()),
+        child: Scaffold(
+          bottomNavigationBar: bNavBar,
+          body: BlocBuilder<ExercisesBloc, ExercisesState>(
+            builder: (context, state) {
+              return state.map(
+                initial: (_) => Container(),
+                loadInProgress: (_) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                loadSuccess: (state) {
+                  return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: MediaQuery.of(context).orientation ==
+                                Orientation.landscape
+                            ? 3
+                            : 2),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: GestureDetector(
+                            onTap: () => bNavBar.changePage(1,
+                                exercise: state.exercises[index]),
+                            child:
+                                ExerciseCard(exercise: state.exercises[index])),
+                      );
+                    },
+                    itemCount: state.exercises.length,
+                  );
+                },
+                loadFailure: (state) {
+                  return Container(
+                    child: Text('FAILURE'),
+                  );
+                },
+              );
+            },
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Router.navigator.pushNamed(Routes.exerciseFormPage,
+                  arguments: ExerciseFormPageArguments(editedExercise: null));
+            },
+            child: Center(child: Icon(Icons.add)),
           ),
         ));
   }
